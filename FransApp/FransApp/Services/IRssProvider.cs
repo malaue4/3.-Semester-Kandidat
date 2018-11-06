2afc42875014ab98a2e30e3a06f069218bea43e3
Marinus <malaue4@gmail.com>
2018-11-06 13:53:35 +0100

    En smule rss baggrunds ting
    Microsoft.Toolkit.Parsers er tilføjet, den kan parse rss (bl.a.)

--- FransApp/FransApp/Services/IRssProvider.cs
+++ FransApp/FransApp/Services/IRssProvider.cs
@@ -1,10 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Toolkit.Parsers.Rss;
 
 namespace FransApp.Services
 {
     interface IRssProvider
     {
+        /*
+        Task<bool> AddItemAsync(T item);
+        Task<bool> UpdateItemAsync(T item);
+        Task<bool> DeleteItemAsync(T item);
+        */
+        Task<RssSchema> GetRssFeedAsync(Uri feedUri);
+        Task<IEnumerable<RssSchema>> GetRssFeedsAsync(bool forceRefresh = false);
     }
 }
--- FransApp/FransApp/Services/MockRssProvider.cs
+++ FransApp/FransApp/Services/MockRssProvider.cs
@@ -1,10 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Toolkit.Parsers.Rss;
 
 namespace FransApp.Services
 {
     class MockRssProvider : IRssProvider
     {
+        public Task<RssSchema> GetRssFeedAsync(Uri feedUri)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<RssSchema>> GetRssFeedsAsync(bool forceRefresh = false)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
