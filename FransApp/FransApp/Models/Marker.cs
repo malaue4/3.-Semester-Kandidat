1924a582dae20bb7c135cabb53b28408a1dbbb74
Marinus <malaue4@gmail.com>
2018-12-13 17:02:50 +0100

    Remove unused solution

--- FransApp/FransApp/Models/Marker.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace FransApp.Models
-{
-    public class Marker
-    {
-        public string Id { get; set; }
-        public string Description { get; set; }
-        //public GPS_Koordinat Location { get; set; }
-    }
-}
